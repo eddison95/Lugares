5bbae25d3dc60c20a393dc0ce4a6bb91eb8608d1
eddison <eddisonmo95@gmail.com>
2022-09-29 19:03:51 -0600

    Version semana 2. Se crea conexcion con Firebase y se autentica usuario

--- app/src/main/java/com/lugares/MainActivity.kt
+++ app/src/main/java/com/lugares/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.lugares
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.lugares.databinding.ActivityMainBinding
@@ -21,5 +24,66 @@ class MainActivity : AppCompatActivity() {
         FirebaseApp.initializeApp(this)      // Debe colocarse siempre que creo un proyecto nuevo
         auth = Firebase.auth                         // Debe colocarse siempre que creo un proyecto nuevo
 
+        binding.btRegister.setOnClickListener{ haceRegistro ()} // Le asigna al boton Register la funcion de registro
+        binding.btLogin.setOnClickListener{ haceLogin ()} // Le asigna al boton Login la funcion de login
+
     }
+
+    private fun haceRegistro(){
+        // Obtenemos informacion que ingresa usuario
+       val email = binding.etEmail.text.toString()  // Obtiene el texto del campo email
+       val clave = binding.etClave.text.toString()  // Obtiene el texto del campo clave
+
+        //Se llama a la funcion para crear un usuario en Firebase (correo/contrasena)
+        auth.createUserWithEmailAndPassword(email,clave)
+            .addOnCompleteListener(this){ task ->
+                val user : FirebaseUser?=null
+                if(task.isSuccessful){ // Si pudo crear el usuario
+                    Log.d("Autenticando usuario","usuario creado")
+                    val user = auth.currentUser // Recupero informacion del usuario creado
+                }else{
+                    Log.d("Autenticando usuario","Error creando usuario")
+                }
+                actualiza(user) // Asigno la variable a la funcion actualiza para validar que el usuario pudo auntenticarse
+
+            }
+
+
+
+    }
+
+    private fun haceLogin(){
+        // Obtenemos informacion que ingresa usuario
+        val email = binding.etEmail.text.toString()  // Obtiene el texto del campo email
+        val clave = binding.etClave.text.toString()  // Obtiene el texto del campo clave
+
+        //Se llama a la funcion para iniciarsesion con un usuario en Firebase (correo/contrasena)
+        auth.signInWithEmailAndPassword(email,clave)
+            .addOnCompleteListener(this){ task ->
+                val user : FirebaseUser?=null
+                if(task.isSuccessful){ // Si pudo autenticar el usuario
+                    Log.d("Autenticando usuario","usuario autenticado")
+                    val user = auth.currentUser // Recupero informacion del usuario creado
+                    actualiza(user) // Asigno la variable a la funcion actualiza para validar que el usuario pudo auntenticarse
+                }else{
+                    Log.d("Autenticando usuario","Error autenticando usuario")
+                    actualiza(null) // Asigno la variable a la funcion actualiza para validar que el usuario pudo auntenticarse
+                }
+
+
+            }
+
+
+    }
+
+    private fun actualiza(user: FirebaseUser?) {
+        // Si hay un usuario definido, se redirije a pantalla principal
+        if (user !=null){
+            // se redirije a pantalla principal
+            val intent = Intent(this,Principal::class.java)
+            startActivity(intent)  // Redirije a pantalla principal
+        }
+    }
+
+    //
 }
